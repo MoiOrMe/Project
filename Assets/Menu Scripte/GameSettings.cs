151a731d88e95cf6621c8f8f50cf63f8aca02307
Xerrrit <104190620+starssilver@users.noreply.github.com>
2024-10-25 14:14:36 +0200

    menu final forme

---
+++ Assets/Menu Scripte/GameSettings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameSettings
+{
+    public static string Difficulty { get; set; } = "Easy"; // Valeur par défaut
+}
+
